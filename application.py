5d392200dd991686262e066b041607fb379d05a8
dgfun <46782895+dgfun@users.noreply.github.com>
2020-07-04 04:30:15 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    print("Handling request to home page.")
+@app.route("/welcome/<username>")
+def welcome(username):
+    
 
-    return "Hello World!"
+    return f"Welcome, {username}"
+
+@app.route("/hello/")
+def hello():
+    return "Hello World"
+
+@app.route("/hey")
+def hey():
+    return "Hey Man"
 
 
 if __name__ == "__main__":
